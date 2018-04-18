449dcce3d386b93110cb80dd69bc2d0a9ca38fc6
Nick Kravchuk <nick@hexbrain.com>
2018-04-18 20:35:10 +0300

    Created index.php file, added HTML structure for displaying 9 images, used Bootstrap classes for that

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo 'Image Gallery' ?></title>
+    </head>
+    <body>
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" alt="Image" src="<?php echo "https://fakeimg.pl/300x200/282828/eae0d0/?retina=1" ?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
