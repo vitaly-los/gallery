a0415e3603d3aa04e678b2af268144981cd94cbf
Los Vitaly <vitalykk1983@gmail.com>
2018-04-18 21:47:20 +0300

    add files

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Title of the document</title>
+    <link rel="stylesheet" href="/main.css">
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
