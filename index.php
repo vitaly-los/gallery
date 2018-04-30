0514d74e20d67db342ad1b0d9666ddff9acfcc81
Nick Kravchuk <nick@hexbrain.com>
2018-04-30 17:17:34 +0300

    array support and foreach cycle for displaying images, reformat code

--- index.php
+++ index.php
@@ -1,88 +1,39 @@
 <?php
-
+//loaded file with variables
 require_once('src/app.php');
 
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title><?php echo PAGE_TITLE ?></title>
-        <link rel="stylesheet" href="pub/css/bootstrap.css">
-        <link rel="stylesheet" href="pub/css/main.css">
-        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
-    </head>
-    <body>
-    <div class="album py-5 bg-light">
-        <div class="container">
-            <h1 class="h1 text-center"><?php echo PAGE_TITLE ?></h1>
-            <div class="row">
+<head>
+    <title><?php echo PAGE_TITLE ?></title>
+    <link rel="stylesheet" href="pub/css/bootstrap.css">
+    <link rel="stylesheet" href="pub/css/main.css">
+    <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+</head>
+<body>
+<div class="album py-5 bg-light">
+    <div class="container">
+        <h1 class="h1 text-center"><?php echo PAGE_TITLE ?></h1>
+        <div class="row">
+            <?php foreach ($images as $image): ?>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
+                        <a data-fancybox="gallery"
+                           href="<?php echo isset($image['url']) ? $image['url'] : IMAGE_PLACEHOLDER ?>">
+                            <img class="card-img-top" alt="Image"
+                                 src="<?php echo isset($image['thumbnail']) ? $image['thumbnail'] : IMAGE_PLACEHOLDER ?>">
                         </a>
+                        <div class="card-body">
+                            <p class="card-text"><?php echo $image['description'] ?></p>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="card mb-4 box-shadow">
-                        <a data-fancybox="gallery" href="<?php echo isset($imageUrl)?$imageUrl:IMAGE_PLACEHOLDER ?>">
-                            <img class="card-img-top" alt="Image" src="<?php echo isset($smallImageUrl)?$smallImageUrl:IMAGE_PLACEHOLDER ?>">
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
-    </body>
+</div>
+</body>
 </html>
--- src/app.php
+++ src/app.php
@@ -4,7 +4,69 @@
 define('PAGE_TITLE', 'Image Gallery');
 /** defined image placeholder  */
 define('IMAGE_PLACEHOLDER', 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1');
-/** @var string small image url $smallImageUrl */
-$smallImageUrl = 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1';
-/** @var string image url $imageUrl */
-$imageUrl = 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1';
+/** @var array with images $images */
+$images = [
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ],
+    [
+        'url' => 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1',
+        'thumbnail' => 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1',
+        'description' => 'Fake IMG',
+        'width' => '3000',
+        'height' => '2000'
+    ]
+];
