4723f6b4702c0e3fc82d7dcdb00a6c8d60109319
Nick Kravchuk <nick@hexbrain.com>
2018-04-30 16:56:54 +0300

    Added app.php filed with declared variables and constants

---
+++ src/app.php
@@ -0,0 +1,10 @@
+<?php
+
+/** defined constant that consists page title  */
+define('PAGE_TITLE', 'Image Gallery');
+/** defined image placeholder  */
+define('IMAGE_PLACEHOLDER', 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1');
+/** @var string small image url $smallImageUrl */
+$smallImageUrl = 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1';
+/** @var string image url $imageUrl */
+$imageUrl = 'https://fakeimg.pl/3000x2000/282828/eae0d0/?retina=1';
