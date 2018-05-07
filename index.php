c58dc3160dbb0cfb22307e5e30fc28b722ba971c
Nick Kravchuk <nick@hexbrain.com>
2018-05-07 12:00:19 +0300

    HomeWork 8: Added function for getting images, checking image path, sorting, getting date, getting image size

--- index.php
+++ index.php
@@ -18,18 +18,20 @@ require_once('src/app.php');
     <div class="container">
         <h1 class="h1 text-center"><?php echo PAGE_TITLE ?></h1>
         <div class="row">
-            <?php if (!empty($images)): ?>
+            <?php if (!empty($images = getImages())): ?>
             <?php foreach ($images as $image): ?>
                 <div class="col-md-4">
                     <div class="card mb-4 box-shadow">
                         <a data-fancybox="gallery"
-                           href="<?php echo isset($image['url']) ? $image['url'] : IMAGE_PLACEHOLDER ?>">
+                           href="<?php echo $image['url'] ?>">
                             <img class="card-img-top" alt="Image"
                                  src="<?php echo $image['url'] ?>">
                         </a>
                         <div class="card-body">
                             <p class="card-text">Author: <?php echo $image['description'] ?>,
-                                Resolution: <?php echo implode('x', [$image['width'], $image['height']]) ?></p>
+                                Resolution: <?php echo implode('x', [$image['width'], $image['height']]) ?>
+                                Created at: <?php echo $image['created_at'] ?>
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/app.php
+++ src/app.php
@@ -8,52 +8,90 @@ define('IMAGE_PLACEHOLDER', 'https://fakeimg.pl/300x200/282828/eae0d0/?retina=1'
 define('IMAGE_RESOURCE_URL', 'https://picsum.photos/list');
 
 /**
- * function sor sorting images
- * @param $imageA
- * @param $imageB
- * @return int
+ * Get image array
+ *
+ * @return array
  */
-function sortImages($imageA, $imageB)
+function getImages()
 {
-    if ($imageA['description'] == $imageB['description']) {
-        return 0;
+    //read file content to string $jsonList, @ means suppress errors in case of problems with service availability, but it's bad practice
+    $jsonList = @file_get_contents(IMAGE_RESOURCE_URL);
+
+    if (!empty($jsonList)) { //check if string is not empty
+        /** @var array with images $images */
+        $images = [];
+
+        //json_decode converts string to array, array_slice select first 9 elements
+        foreach (array_slice(json_decode($jsonList, true), 0, 9) as $key => $value) {
+            /*
+             * set new array element
+             *
+             * NOTE: Here is one issue: url https://picsum.photos/0016_gkT4FfgHO5o.jpeg
+             * doesn't return image that exists on page https://unsplash.com/photos/gkT4FfgHO5o
+             * so if we use one of that solution it will display wrong image. So we need to extract correct images by some rule:
+             * The idea is finding some pattern in source page (https://unsplash.com/photos/gkT4FfgHO5o) and getting correct link
+             *
+             * found url with direct link to image in source of page https://unsplash.com/photos/gkT4FfgHO5o/download?force=true
+             * alternative solution is using https://picsum.photos/500/500/?image=16 URL
+            */
+            $image = imageExists($value['post_url'] . '/download');
+            list($width, $height) = getimagesize($image);
+            $images[] = [
+                'url' => $image,
+                'thumbnail' => $value['post_url'] . '/download',
+                'description' => $value['author'],
+                'width' => $width,
+                'height' => $height,
+                'created_at' => getCurrentDate()
+            ];
+        }
+    } else {
+        // set empty array
+        $images = [];
     }
-    return ($imageA['description'] < $imageB['description']) ? -1 : 1;
+    sortImages($images);
+
+    return $images;
 }
 
-//read file content to string $jsonList, @ means suppress errors in case of problems with service availability, but it's bad practice
-$jsonList = @file_get_contents(IMAGE_RESOURCE_URL);
-
-if (!empty($jsonList)) { //check if string is not empty
-    /** @var array with images $images */
-    $images = [];
-
-    //json_decode converts string to array, array_slice select first 9 elements
-    foreach (array_slice(json_decode($jsonList, true), 0, 9) as $key => $value) {
-        /*
-         * set new array element
-         *
-         * NOTE: Here is one issue: url https://picsum.photos/0016_gkT4FfgHO5o.jpeg or https://picsum.photos/500/500/?image=16
-         * doesn't return image that exists on page https://unsplash.com/photos/gkT4FfgHO5o
-         * so if we use one of that solution it will display wrong image. So we need to extract correct images by some rule:
-         * The idea is finding some pattern in source page (https://unsplash.com/photos/gkT4FfgHO5o) and getting correct link
-         *
-         * found url with direct link to image in source of page https://unsplash.com/photos/gkT4FfgHO5o/download?force=true
-        */
-        $images[] = [
-            'url' => $value['post_url'] . '/download',
-            'thumbnail' => $value['post_url'] . '/download',
-            'description' => $value['author'],
-            'width' => $value['width'],
-            'height' => $value['height']
-        ];
+/**
+ * Sort array of images
+ * @param $images
+ */
+function sortImages(&$images)
+{
+    if (!empty($images)) {
+        //sorting part
+        usort($images, function ($imageA, $imageB) {
+            if ($imageA['description'] == $imageB['description']) {
+                return 0;
+            }
+            return ($imageA['description'] < $imageB['description']) ? -1 : 1;
+        });
     }
-} else {
-    // set empty array
-    $images = [];
 }
 
-if (!empty($images)) {
-//sorting part
-    usort($images, 'sortImages');
+/**
+ * Get formatted current time
+ *
+ * @return false|string
+ */
+function getCurrentDate()
+{
+    return date('d M Y H:i:s', time());
+}
+
+/**
+ * Return image page or placeholder
+ *
+ * @param $imagePath
+ * @return string
+ */
+function imageExists($imagePath)
+{
+    if (isset($imagePath) && !empty($imagePath)) {
+        return $imagePath;
+    } else {
+        return IMAGE_PLACEHOLDER;
+    }
 }
