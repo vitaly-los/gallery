9a887aeb7867219b02c03786743f332e4b4fa49d
Los Vitaly <vitalykk1983@gmail.com>
2018-05-08 09:38:44 +0300

    Add file functions.php

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * In our case we use images from picsum.photos so function check server status
+ * @return boolean
+ */
+function checkServerStatus($content)
+{
+    file_get_contents($content);
+    if (in_array('HTTP/1.1 200 OK', $http_response_header))
+        return true;
+    else
+        return false;
+}
+
+/**
+ * @param string list of images in JSON format
+ * @return array
+ */
+function imagesList(string $content)
+{
+
+    $imagesList = [];
+    $res = file_get_contents($content);
+    $json = json_decode($res, true);
+
+    foreach ($json as $key => $value){
+        $imagesList[] = [
+            'width' => $value['width'],
+            'height' => $value['height'],
+            'id' => $value['id'],
+            //'author' => $value['author']
+        ];
+    }
+    return $imagesList;
+}
