1ced10114f5de946f822d6fcd412f29baced4dee
Nick Kravchuk <nick@hexbrain.com>
2018-05-09 22:19:59 +0300

    HomeWork 9: modified form, now there is only required fields

--- view/form.php
+++ view/form.php
@@ -8,69 +8,19 @@
     <body>
         <div class="album py-5 bg-light">
             <div class="container">
-                <h1 class="h1 text-center">Form Example</h1>
+                <h1 class="h1 text-center">Upload New Image</h1>
                 <form action="/submit" method="post" enctype="multipart/form-data">
                     <div class="form-group">
-                        <label for="exampleFormControlInput1">Text</label>
-                        <input type="text" class="form-control" name="exampleFormControlInput1">
+                        <label for="authorname">Author Name</label>
+                        <input type="text" class="form-control" id="authorname" name="authorname">
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlSelect1">Example select</label>
-                        <select class="form-control" name="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
+                        <label for="description">Description</label>
+                        <textarea class="form-control" id="description" name="description" rows="3"></textarea>
                     </div>
                     <div class="form-group">
-                        <label for="exampleFormControlSelect2">Example multiple select</label>
-                        <select multiple class="form-control" name="exampleFormControlSelect2">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" name="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" name="defaultCheck1">
-                        <label class="form-check-label" for="defaultCheck1">
-                            Default checkbox
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" name="defaultCheck2" disabled>
-                        <label class="form-check-label" for="defaultCheck2">
-                            Disabled checkbox
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" name="exampleRadios1" value="option1" checked>
-                        <label class="form-check-label" for="exampleRadios1">
-                            Default radio
-                        </label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="exampleRadios" name="exampleRadios2" value="option2">
-                        <label class="form-check-label" for="exampleRadios2">
-                            Second default radio
-                        </label>
-                    </div>
-                    <div class="form-check disabled">
-                        <input class="form-check-input" type="radio" name="exampleRadios" name="exampleRadios3" value="option3" disabled>
-                        <label class="form-check-label" for="exampleRadios3">
-                            Disabled radio
-                        </label>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlFile1">Example file input</label>
-                        <input type="file" class="form-control-file" name="exampleFormControlFile1">
+                        <label for="image">Select Image</label>
+                        <input type="file" class="form-control-file" id="image" name="image">
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-10">
