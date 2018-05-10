c6015cdd8ca376687b344171e9eb669096b165d9
Los Vitaly <vitalykk1983@gmail.com>
2018-05-10 23:29:59 +0300

    Change index.php

--- index.php
+++ index.php
@@ -5,6 +5,25 @@ require_once './functions.php';
 ?>
 
 <?php
+// URL for images
+$content = 'https://picsum.photos/list';
+
+// If external server not work and we receive empty result kill script
+if (checkServerStatus($content) == false)
+    die('Ooops something went wrong');
+
+// Set returned array for variable
+$images = imagesList($content);
+
+// Set number of images on page
+$imagesOnPage = 9;
+?>
+<div class="container">
+    <a href="./index.php"><button type="button" class="btn btn-primary">HomePage</button></a>
+    <a href="./index.php?page=gallery"> <button type="button" class="btn btn-secondary">View Gallery</button></a>
+    <a href="./index.php?page=form"><button type="button" class="btn btn-success">Upload Image</button></a>
+</div>
+<?php
 
 // Filter paramert's from GET form
 $page = (string)filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
@@ -22,19 +41,6 @@ switch ($page){
 
 ?>
 
-<?php
-// URL for images
-$content = 'https://picsum.photos/list';
 
-// If external server not work and we receive empty result kill script
-if (checkServerStatus($content) == false)
-    die('Ooops something went wrong');
-
-// Set returned array for variable
-$images = imagesList($content);
-
-// Set number of images on page
-$imagesOnPage = 9;
-?>
 
 <?php require_once './template/footer.php'; ?>
