7ef74aefdaf84fffabb6825a56a828e0721ed046
Los Vitaly <vitalykk1983@gmail.com>
2018-05-10 22:45:47 +0300

    Change header.php

---
+++ template/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title ?? 'Demo Gallery'; ?></title>
+        <link rel="stylesheet" href="./main.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+        <link href="bootstrap-3.3.2/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+ 
